6bf01d4775e8121c8d5579cac1915b910a111de1
ultroNeous2021 <92087111+ultroNeous2021@users.noreply.github.com>
2021-11-23 15:12:24 +0530

    Backend with Laravel

---
+++ Models/Category.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * File name: Category.php
+ * Last modified: 2021.05.29 at 08:34:22
+ * Author: ultroNeous Technologies https://ultroneous.com/
+ * Copyright (c) 2021
+ */
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Models\Media;
+
+/**
+ * Class Category
+ * @package App\Models
+ * @version April 11, 2020, 1:57 pm UTC
+ *
+ * @property path\Collection Food
+ * @property string name
+ * @property string description
+ */
+class Category extends Model implements HasMedia
+{
+    use HasMediaTrait {
+        getFirstMediaUrl as protected getFirstMediaUrlTrait;
+    }
+
+    public $table = 'categories';
+    
+
+
+    public $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string',
+        'image' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required',
+        'description' => 'required'
+    ];
+
+    /**
+     * New Attributes
+     *
+     * @var array
+     */
+    protected $appends = [
+        'custom_fields',
+        'has_media'
+        
+    ];
+
+    /**
+     * @param Media|null $media
+     * @throws path\InvalidManipulation
+     */
+    public function registerMediaConversions(Media $media = null)
+    {
+        $this->addMediaConversion('thumb')
+            ->fit(Manipulations::FIT_CROP, 200, 200)
+            ->sharpen(10);
+
+        $this->addMediaConversion('icon')
+            ->fit(Manipulations::FIT_CROP, 100, 100)
+            ->sharpen(10);
+    }
+
+    public function customFieldsValues()
+    {
+        return $this->morphMany('App\Models\CustomFieldValue', 'customizable');
+    }
+
+    /**
+     * to generate media url in case of fallback will
+     * return the file type icon
+     * @param string $conversion
+     * @return string url
+     */
+    public function getFirstMediaUrl($collectionName = 'default',$conversion = '')
+    {
+        $url = $this->getFirstMediaUrlTrait($collectionName);
+        $array = explode('.', $url);
+        $extension = strtolower(end($array));
+        if (in_array($extension,config('medialibrary.extensions_has_thumb'))) {
+            return asset($this->getFirstMediaUrlTrait($collectionName,$conversion));
+        }else{
+            return asset(config('medialibrary.icons_folder').'/'.$extension.'.png');
+        }
+    }
+
+    public function getCustomFieldsAttribute()
+    {
+        $hasCustomField = in_array(static::class,setting('custom_field_models',[]));
+        if (!$hasCustomField){
+            return [];
+        }
+        $array = $this->customFieldsValues()
+            ->join('custom_fields','custom_fields.id','=','custom_field_values.custom_field_id')
+            ->where('custom_fields.in_table','=',true)
+            ->get()->toArray();
+
+        return convertToAssoc($array,'name');
+    }
+
+    /**
+     * Add Media to api results
+     * @return bool
+     */
+    public function getHasMediaAttribute()
+    {
+        return $this->hasMedia('image') ? true : false;
+    }
+
+    /**
+     * @return path\HasMany
+     **/
+    public function foods()
+    {
+        return $this->hasMany(\App\Models\Food::class, 'category_id');
+    }
+
+    /**
+     * @return path\BelongsToMany
+     **/
+    public function restaurants()
+    {
+        return $this->belongsToMany(\App\Models\Restaurant::class, 'foods');
+    }
+
+
+}
